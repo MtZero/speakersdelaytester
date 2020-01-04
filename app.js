7bcf4d56d075d803bc71ee15682d1b11d122b496
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2020-01-04 17:52:48 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+window.addEventListener('click', init);
+var cnt = 0;
+var stdTime = (new Date()).getTime();
+
+function init() {
+    window.removeEventListener("click", init);
+    document.getElementById("tts").hidden = true;
+    document.getElementById("delay").hidden = false;
+    setInterval(run, 800);
+    stdTime = (new Date()).getTime();
+    window.addEventListener("click", touch)
+}
+
+function run() {
+    stdTime = (new Date()).getTime() + 400;
+    if (cnt === 0) beep(0.39, 0.02, 1000);
+    else beep(0.39, 0.02, 800);
+    cnt = (cnt + 1) % 4;
+}
+
+function touch() {
+    var touchTime = (new Date()).getTime();
+    document.getElementById("delay").innerText = (touchTime - stdTime) + "ms";
+}
+
+function beep(start, duration, freq) {
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioCtx = new AudioContext();
+
+    // create Oscillator and gain node
+    var oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+
+    // connect oscillator to gain node to speakers
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    oscillator.detune.value = 100; // value in cents
+    oscillator.start(start);
+    oscillator.stop(start+duration);
+
+    oscillator.frequency.value = freq;
+    gainNode.gain.value = 0.5;
+}
